c1f5b29a11cff7f95a25495563f94b070e97faf1
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-05-13 09:38:52 +0200

    initial commit, install dependencies

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App__h">Moja nowa apka</h1>
+      <p className="App__p">Ktora dziala</p>
+    </div>
+  );
+}
+
+export default App;

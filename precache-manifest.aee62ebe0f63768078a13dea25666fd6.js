75b50fafe19f7daebbe0a21a6f50571deedaf777
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-05-17 13:50:30 +0200

    Updates

---
+++ precache-manifest.aee62ebe0f63768078a13dea25666fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1d1a9aff2a7c0fa6bee60d50f656f2",
+    "url": "/Shopping-List/index.html"
+  },
+  {
+    "revision": "51798e17f91d9572fce1",
+    "url": "/Shopping-List/static/css/main.34433e35.chunk.css"
+  },
+  {
+    "revision": "70923f3c7b174b80e016",
+    "url": "/Shopping-List/static/js/2.a8373a97.chunk.js"
+  },
+  {
+    "revision": "51798e17f91d9572fce1",
+    "url": "/Shopping-List/static/js/main.59102e2e.chunk.js"
+  },
+  {
+    "revision": "0cacf55891353fe3a6a1",
+    "url": "/Shopping-List/static/js/runtime~main.134e1c1a.js"
+  }
+]);
